41a02489ff5fceb287574320049ca73e1ca9d6a2
tiagolimarocha <tiago.rocha@radically.digital>
2022-07-26 11:04:22 +0100

    fixed numerous issues

--- src/main/scala/com/playground/tiago/finch/todo/Main.scala
+++ src/main/scala/com/playground/tiago/finch/todo/Main.scala
@@ -1,4 +1,4 @@
-package  com.playground.tiago.finch.todo
+package com.playground.tiago.finch.todo
 
 import com.twitter.finagle.{Http, ListeningServer}
 import com.twitter.util.Future
@@ -17,16 +17,19 @@ import doobie.util.transactor.Transactor
 import doobie._
 import doobie.implicits._
 
-import com.playground.tiago.todo.models.Todo
+import _root_.com.playground.tiago.finch.todo.models.Todo
 
 object Main extends IOApp {
-  implicit val cs = IO.contextShift(ExecutionContexts.syncronous)
+  implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
 
   val xa = Transactor.fromDriverManager[IO](
-    "org.sqlite.JDBC", "jdbc:sqlite:data.db", "", "",
-    Blocker.liftExecutionContext(ExecutionContexts.syncronous)
+    "org.sqlite.JDBC",
+    "jdbc:sqlite:data.db",
+    "",
+    "",
+    Blocker.liftExecutionContext(ExecutionContexts.synchronous)
   )
-  
+
   val root: String = "todos"
 
   val createDb = sql"""
@@ -36,17 +39,14 @@ object Main extends IOApp {
         description TEXT,
         done NUMERIC
       )
-    """
-    .update
-    .run
+    """.update.run
 
-  val create: Endpoint[IO, Todo] = post(root :: jsonBody[Todo]) {
-    todo: Todo => for {
-      id <- sql"INSERT INTO todo (name, description, done) 
-        VALUES (${todo.name}, ${todo.description}, ${todo.done})"
-        .update
-        .withUniqueGeneralKeys[Int]("id")
-        .transact(xa)
+  val create: Endpoint[IO, Todo] = post(root :: jsonBody[Todo]) { todo: Todo =>
+    for {
+      id <-
+        sql"INSERT INTO todo (name, description, done)  VALUES (${todo.name}, ${todo.description}, ${todo.done})".update
+          .withUniqueGeneratedKeys[Int]("id")
+          .transact(xa)
 
       created <- sql"SQL * FROM todo WHERE id = $id"
         .query[Todo]
@@ -55,38 +55,35 @@ object Main extends IOApp {
     } yield Created(created)
   }
 
-   val update: Endpoint[IO, Todo] = put(root :: path[Int] :: jsonBody[Todo]) {
-    (id: Int, todo: Todo) => for {
-      _ <- sql"UPDATE todo SET 
-          name = ${todo.name},
-          todo = ${todo.description},
-          done = ${todo.done}
-        WHERE id = $id"
+  val update: Endpoint[IO, Todo] = put(root :: path[Int] :: jsonBody[Todo]) {
+    (id: Int, todo: Todo) =>
+      for {
+        _ <-
+          sql"UPDATE todo SET name = ${todo.name}, todo = ${todo.description}, done = ${todo.done} WHERE id = $id".update.run
+            .transact(xa)
 
-      todo <- sql"SELECT * FROM todo WHERE id = $id"
-        .query[Todo]
-        .unique
-        .transact(xa)
-    } yield Ok(todo)
+        todo <- sql"SELECT * FROM todo WHERE id = $id"
+          .query[Todo]
+          .unique
+          .transact(xa)
+      } yield Ok(todo)
   }
 
-  val delete: Endpoint[IO, Todo] = delete(root :: path[Int]) {
-    id: Int => for {
-      _ <- sql"DELETE FROM todo WHERE id = $id"
-        .update
-        .run
+  val delete: Endpoint[IO, Unit] = delete(root :: path[Int]) { id: Int =>
+    for {
+      _ <- sql"delete from todo where id = $id".update.run
         .transact(xa)
     } yield NoContent
   }
 
-  val findOne: Endpoint[IO, Todo] = get(root :: path[Int]) {
-    id: Int => for {
+  val findOne: Endpoint[IO, Todo] = get(root :: path[Int]) { id: Int =>
+    for {
       todos <- sql"SELECT * FROM todo WHERE id = $id"
         .query[Todo]
         .to[Set]
         .transact(xa)
     } yield todos.headOption match {
-      case None => NotFound(new Exception("Record not found"))
+      case None       => NotFound(new Exception("Record not found"))
       case Some(todo) => Ok(todo)
     }
   }
@@ -100,16 +97,19 @@ object Main extends IOApp {
     } yield Ok(todos)
   }
 
-  def startServer: IO[ListeningServer] = createDb.transact(xa).flatMap { 
-    _ => IO(Http.server.serve(
-      ":8081", 
-      (create :+: update :+: delete :+: findOne :+: findMany).toServiceAs[Application.Json]
-    ))
+  def startServer: IO[ListeningServer] = createDb.transact(xa).flatMap { _ =>
+    IO(
+      Http.server.serve(
+        ":8081",
+        (create :+: update :+: delete :+: findOne :+: findMany)
+          .toServiceAs[Application.Json]
+      )
+    )
   }
 
   def run(args: List[String]): IO[ExitCode] = {
-    val server = Resource.make(startServer)(
-      s => IO.suspend(implicitly[ToAsync[Future, IO]].apply(s.close()))
+    val server = Resource.make(startServer)(s =>
+      IO.suspend(implicitly[ToAsync[Future, IO]].apply(s.close()))
     )
 
     server.use(_ => IO.never).as(ExitCode.Success)
